0c675baef3c614e5f0736bb67b23ecfb195a9f4e
Amrita-Patel <amritapatel425@gmail.com>
2023-12-14 16:00:31 +0530

    Project Added

---
+++ src/main/java/org/example/VideocallApplication.java
@@ -0,0 +1,46 @@
+package org.example;
+
+
+
+    import org.example.user.User;
+import org.example.user.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+    @SpringBootApplication
+    public class VideocallApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(VideocallApplication.class, args);
+        }
+
+        @Bean
+        public CommandLineRunner commandLineRunner(
+                UserService service
+        ) {
+            return args -> {
+                service.register(User.builder()
+                        .username("Ali")
+                        .email("ali@mail.com")
+                        .password("aaa")
+                        .build());
+
+                service.register(User.builder()
+                        .username("John")
+                        .email("john@mail.com")
+                        .password("aaa")
+                        .build());
+
+                service.register(User.builder()
+                        .username("Anny")
+                        .email("anna@mail.com")
+                        .password("aaa")
+                        .build());
+            };
+        }
+
+    }
+
+
